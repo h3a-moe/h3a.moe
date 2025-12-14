5249344a31d727ac22138b29987818b09b8c7808
H3arn <root@h3a.moe>
2025-12-15 02:28:36 +0800

    feat: give my motto a line break, in response to papermod version bump

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ git submodule update --init --recursive
 
 sed -i 's/<span>{{ .subtitle | markdownify }}<\/span>/<span style="font-size:90%">{{ .subtitle | markdownify }}<\/span>/g' themes/PaperMod/layouts/partials/index_profile.html
 
-grep -rl 'site.Social' . | xargs sed -i '' 's/site\.Social/site.Params.Social/g'
+#grep -rl 'site.Social' . | xargs sed -i '' 's/site\.Social/site.Params.Social/g'
 
 hugo -D
 
