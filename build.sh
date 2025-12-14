e09f5ab05d2322237a3a9429b12b4f80244e22fe
H3arn <root@h3a.moe>
2025-12-15 02:41:31 +0800

    feat: remove subtile resize 90% back to 100%

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 git submodule update --init --recursive
 
-sed -i 's/<span>{{ .subtitle | markdownify }}<\/span>/<span style="font-size:90%">{{ .subtitle | markdownify }}<\/span>/g' themes/PaperMod/layouts/partials/index_profile.html
+#sed -i 's/<span>{{ .subtitle | markdownify }}<\/span>/<span style="font-size:90%">{{ .subtitle | markdownify }}<\/span>/g' themes/PaperMod/layouts/partials/index_profile.html
 
 #grep -rl 'site.Social' . | xargs sed -i '' 's/site\.Social/site.Params.Social/g'
 
